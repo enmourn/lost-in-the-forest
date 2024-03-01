d670d39bc7ab662d0be6ad3455ca2b7c03c4f188
enmourn <enmourn@gmail.com>
2024-03-01 11:39:58 +0300

    keys q & e to move

--- src/components/Keyboard.ts
+++ src/components/Keyboard.ts
@@ -1,31 +1,57 @@
 export default class Keyboard {
-  left: boolean
-  right: boolean
-  up: boolean
-  down: boolean
-  g: boolean
+  a: boolean = false
+  w: boolean = false
+  s: boolean = false
+  d: boolean = false
+  arrowLeft: boolean = false
+  arrowUp: boolean = false
+  arrowRight: boolean = false
+  arrowDown: boolean = false
+  q: boolean = false
+  e: boolean = false
+  g: boolean = false
+
+  left: boolean = false
+  up: boolean = false
+  right: boolean = false
+  down: boolean = false
 
   constructor() {
-    this.left = false
-    this.right = false
-    this.up = false
-    this.down = false
-    this.g = false
-
     document.onkeydown = (e) => {
-      if (e.code === "KeyA" || e.code === "ArrowLeft") this.left = true
-      if (e.code === "KeyD" || e.code === "ArrowRight") this.right = true
-      if (e.code === "KeyW" || e.code === "ArrowUp") this.up = true
-      if (e.code === "KeyS" || e.code === "ArrowDown") this.down = true
+      if (e.code === "KeyA") this.a = true
+      if (e.code === "KeyW") this.w = true
+      if (e.code === "KeyS") this.s = true
+      if (e.code === "KeyD") this.d = true
+      if (e.code === "ArrowLeft") this.arrowLeft = true
+      if (e.code === "ArrowUp") this.arrowUp = true
+      if (e.code === "ArrowDown") this.arrowDown = true
+      if (e.code === "ArrowRight") this.arrowRight = true
+      if (e.code === "KeyQ") this.q = true
+      if (e.code === "KeyE") this.e = true
       if (e.code === "KeyG") this.g = true
+      this.updDirection()
     }
     document.onkeyup = (e) => {
-      if (e.code === "KeyA" || e.code === "ArrowLeft") this.left = false
-      if (e.code === "KeyD" || e.code === "ArrowRight") this.right = false
-      if (e.code === "KeyW" || e.code === "ArrowUp") this.up = false
-      if (e.code === "KeyS" || e.code === "ArrowDown") this.down = false
+      if (e.code === "KeyA") this.a = false
+      if (e.code === "KeyW") this.w = false
+      if (e.code === "KeyS") this.s = false
+      if (e.code === "KeyD") this.d = false
+      if (e.code === "ArrowLeft") this.arrowLeft = false
+      if (e.code === "ArrowUp") this.arrowUp = false
+      if (e.code === "ArrowDown") this.arrowDown = false
+      if (e.code === "ArrowRight") this.arrowRight = false
+      if (e.code === "KeyQ") this.q = false
+      if (e.code === "KeyE") this.e = false
       if (e.code === "KeyG") this.g = false
+      this.updDirection()
     }
+  }
+
+  private updDirection() {
+    this.left = this.a || this.q
+    this.up = this.w || this.q || this.e
+    this.down = this.s
+    this.right = this.d || this.e
   }
 
   pressOnce(key: string, callback: () => void) {
